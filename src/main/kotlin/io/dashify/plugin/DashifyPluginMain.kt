7d19a5ca47956e05840e4b464e4a99d39b2db5ec
pybsh <rootpi@icloud.com>
2023-05-23 22:54:23 +0900

    :construction: Changes by zeus8

--- src/main/kotlin/io/dashify/plugin/DashifyPluginMain.kt
+++ src/main/kotlin/io/dashify/plugin/DashifyPluginMain.kt
@@ -3,6 +3,10 @@ package io.dashify.plugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class DashifyPluginMain : JavaPlugin() {
+    companion object {
+        lateinit var plugin: JavaPlugin
+    }
+
     override fun onEnable() {
         plugin = this
         saveDefaultConfig()
