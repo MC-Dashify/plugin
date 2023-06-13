4954af986167d2f1176fc7c8098cfc8c1cd390b2
aroxu <30624945+aroxu@users.noreply.github.com>
2023-06-13 19:58:00 +0900

    :art: 반환값 표시 수정

--- src/main/kotlin/io/dashify/plugin/router/Dashify.kt
+++ src/main/kotlin/io/dashify/plugin/router/Dashify.kt
@@ -1,10 +1,10 @@
 package io.dashify.plugin.router
 
-import io.dashify.plugin.util.ConfigHandler
-import io.dashify.plugin.manager.PlayerManager
-import io.dashify.plugin.manager.WorldManager
 import io.dashify.plugin.DashifyPluginMain.Companion.plugin
+import io.dashify.plugin.manager.PlayerManager
 import io.dashify.plugin.manager.RuntimeManager
+import io.dashify.plugin.manager.WorldManager
+import io.dashify.plugin.util.ConfigHandler
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
@@ -33,54 +33,93 @@ fun Application.dashify() {
 
     routing {
         get("/") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
-            call.response.status(HttpStatusCode.OK)
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
+            call.respond(HttpStatusCode.OK, hashMapOf("status" to "ok"))
         }
 
         get("/worlds") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
             call.respond(HttpStatusCode.OK, WorldManager.getWorldsList())
         }
         get("/worlds/{uuid}") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
 
             val result = WorldManager.getWorldInfo(call.parameters["uuid"]!!)
-            if (result["error"] == null) { call.respond(HttpStatusCode.OK, result) }
-            else { call.respond(result["statusCode"] as HttpStatusCode, result) }
+            if (result["error"] == null) {
+                call.respond(HttpStatusCode.OK, result)
+            } else {
+                call.respond(result["statusCode"] as HttpStatusCode, result)
+            }
         }
 
         get("/players") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
             call.respond(HttpStatusCode.OK, PlayerManager.getPlayerList())
         }
         get("/players/{uuid}") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
 
             val result = PlayerManager.getPlayerInfo(call.parameters["uuid"]!!)
-            if (result["error"] == null) { call.respond(HttpStatusCode.OK, result) }
-            else { call.respond(result["statusCode"] as HttpStatusCode, result) }
+            if (result["error"] == null) {
+                call.respond(HttpStatusCode.OK, result)
+            } else {
+                call.respond(result["statusCode"] as HttpStatusCode, result)
+            }
         }
         post("/players/{uuid}/kick") {
-            if (!checkIsEnabled()) return@post call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@post call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
 
             val result = PlayerManager.managePlayer("kick", call.parameters["uuid"]!!, call.receiveText())
-            if (result["error"] == null) { call.response.status(HttpStatusCode.OK) }
-            else { call.respond(result["statusCode"] as HttpStatusCode, result) }
+            if (result["error"] == null) {
+                call.response.status(HttpStatusCode.OK)
+            } else {
+                call.respond(result["statusCode"] as HttpStatusCode, result)
+            }
         }
         post("/players/{uuid}/ban") {
-            if (!checkIsEnabled()) return@post call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@post call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
 
             val result = PlayerManager.managePlayer("ban", call.parameters["uuid"]!!, call.receiveText())
-            if (result["error"] == null) { call.response.status(HttpStatusCode.OK) }
-            else { call.respond(result["statusCode"] as HttpStatusCode, result) }
+            if (result["error"] == null) {
+                call.response.status(HttpStatusCode.OK)
+            } else {
+                call.respond(result["statusCode"] as HttpStatusCode, result)
+            }
         }
 
         get("/tps") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
             call.respond(HttpStatusCode.OK, plugin.server.tps)
         }
         get("/jvm") {
-            if (!checkIsEnabled()) return@get call.respond(HttpStatusCode.fromValue(418), "I'm a tea pot :3")
+            if (!checkIsEnabled()) return@get call.respond(
+                HttpStatusCode.fromValue(418),
+                hashMapOf("status" to "I'm a tea pot :3", "detail" to "server disabled plugin.")
+            )
             call.respond(HttpStatusCode.OK, RuntimeManager.getMemory())
         }
     }
