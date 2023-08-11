04053c499ea86e18b93ac1ef99d666e9996aa021
pybsh <rootpi@icloud.com>
2023-08-11 18:22:19 +0900

    :sparkles: Add Sentry

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly(kotlin("stdlib-jdk8:1.8.21"))
     compileOnly("org.mindrot:jbcrypt:0.4")
+    compileOnly("io.sentry:sentry:6.25.2")
 
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
--- src/main/kotlin/cc/dashify/plugin/DashifyPluginMain.kt
+++ src/main/kotlin/cc/dashify/plugin/DashifyPluginMain.kt
@@ -2,6 +2,7 @@ package cc.dashify.plugin
 
 import cc.dashify.plugin.util.ConfigHandler
 import cc.dashify.plugin.util.StringUtil
+import io.sentry.Sentry
 import org.bukkit.plugin.java.JavaPlugin
 import org.mindrot.jbcrypt.BCrypt
 
@@ -24,6 +25,11 @@ class DashifyPluginMain : JavaPlugin() {
         if (ConfigHandler["key"].toString().trim() == "") {
             val key = BCrypt.hashpw(StringUtil.generateRandomString(64), BCrypt.gensalt())
             ConfigHandler["key"] = key
+        }
+
+        Sentry.init { options ->
+            options.dsn = "https://77d0f9b5a715c78118e3cff7a4217dc1@o4505685304934400.ingest.sentry.io/4505685420670976"
+            options.tracesSampleRate = 1.0
         }
 
         startKtor()
